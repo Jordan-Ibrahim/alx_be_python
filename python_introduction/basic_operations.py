abc7bc17835d77671578b6c3d03dc588c3ae875d
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-07-12 06:05:56 +0100

    Added basic operations filr

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,4 @@
+number1,number2 = 10, 5 # Basic arithmetic operations
+print("Addition of 10 and 5 is", number1 + number2)        # Output: 15
+print("Subtraction of 10 and 5 is", number1 - number2)     # Output: 5
+print("Multiplication of 10 and 5 is", number1 * number2)        # Output: 50
