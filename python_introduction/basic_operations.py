39e4ea08a6cbb04dade1f151b68d1ac4a925e294
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-07-12 06:11:40 +0100

    included variables

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,5 @@
-number1,number2 = 10, 5 # Basic arithmetic operations
+number1 = 10 
+number2 = 5 # Basic arithmetic operations
 print("Addition of 10 and 5 is", number1 + number2)        # Output: 15
 print("Subtraction of 10 and 5 is", number1 - number2)     # Output: 5
 print("Multiplication of 10 and 5 is", number1 * number2)        # Output: 50
