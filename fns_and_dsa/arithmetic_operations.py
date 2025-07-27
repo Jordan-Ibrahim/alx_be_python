58c13e70b646ec01ba63d0b524de4fc077452583
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-07-28 00:11:11 +0100

    added arithmetic operations fle

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,7 @@
 def perform_operation (num1, num2, operation):
-    num1=float(num1)
-    num2=float(num2)
-    operation=str(operation)
+    num1 = float(num1)
+    num2 = float(num2)
+    operation = str(operation)
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
