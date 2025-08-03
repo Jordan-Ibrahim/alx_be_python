949b28b8400f466b42c8a43c41ee1a546d39a7ad
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-08-03 19:29:18 +0100

    Bank account file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -20,5 +20,5 @@ class BankAccount:
             raise ValueError("Withdrawal amount must be positive.")
         
     def display_balance(self):
-        output = f"Current Balance: ${self.account_balance:.2f}"
-        print (output)
+        print = (f"Current Balance: ${self.account_balance:.2f}")
+        
